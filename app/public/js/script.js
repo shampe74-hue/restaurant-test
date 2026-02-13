22195568cbe7fb8e3c1883fb3b9f3770c38be5d5
Jean Rene <jeanrsh.pro@gmail.com>
2026-02-14 00:57:13 +0100

    mise à jour mineur

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -2,6 +2,13 @@ const nav = document.querySelector("nav");
 const openBtn = document.querySelector(".header-btn");
 const closeBtn = document.querySelector(".header-close");
 
+const btnAside = document.querySelector(".btn-nav-second");
+const navAside = document.querySelector(".nav-aside");
+
+btnAside.addEventListener("click", () => {
+  navAside.classList.toggle("open");
+});
+
 openBtn.addEventListener("click", () => {
   nav.classList.add("open");
 });
